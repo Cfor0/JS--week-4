992c1b18e210db2d0cae961e3559fd63b78f966d
cfor0 <cfor0@outlook.com>
2020-11-16 15:23:43 -0500

    finished easy exercise

--- easy/script.js
+++ easy/script.js
@@ -1,1 +1,27 @@
-console.log("hi")
+
+
+// Create two test arrays
+let arr = [1, 2, 3, 4, 5, 6, 7]
+let arr2 = [23, 34, 123, 5647, 3]
+let arr3 = [10, 5]
+
+// Declare variable for all added numbers
+let addNums = 0;
+
+//Create function that adds each number in the array
+const numAvg = (a) => {
+
+    // Iterate through each number in the array
+    for (let i = 0; i < a.length; i++) {
+        // Add current number to total
+        addNums += a[i];
+
+        //When last number is reached, return the median.
+        if (i === a.length -1) {
+            return console.log(`The average in this array is ${addNums/ a.length}`)
+        }
+        
+    }
+}
+
+numAvg(arr3)
